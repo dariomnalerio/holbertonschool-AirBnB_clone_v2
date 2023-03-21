8e526faf88a7dd477975bd50ae95267d947fd9ef
dariomnalerio <dariomnalerio@gmail.com>
2023-03-21 18:43:44 -0300

    Update city, state and db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
 from models.base_model import Base
 from models.user import User
 from models.place import Place
@@ -9,11 +9,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-USER = os.environ.get('HBNB_MYSQL_USER')
-PSWD = os.environ.get('HBNB_MYSQL_PWD')
-HOST = os.environ.get('HBNB_MYSQL_HOST')
-DB = os.environ.get('HBNB_MYSQL_DB')
-ENV = os.environ.get('HBNB_ENV')
+
 
 class DBStorage():
     """Class method"""
@@ -21,7 +17,11 @@ class DBStorage():
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            USER, PSWD, HOST, DB), pool_pre_ping=True)
+        self.__engine =  self.__engine = create_engine(('mysql+mysqldb://{}:{}@{}/{}')
+                                        .format(getenv('HBNB_MYSQL_USER'),
+                                                getenv('HBNB_MYSQL_PWD'),
+                                                getenv('HBNB_MYSQL_HOST'),
+                                                getenv('HBNB_MYSQL_DB')),
+                                        pool_pre_ping=True)
         if ENV == 'test':
             Base.metadata.drop_all(self.__engine)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
+
+    else:
+        @property
+        def cities(self):
+            """
+            returns a list of City instances with
+            state_id matching the current State.id
+            """
+            from models import storage
+            cities = storage.all("City")
+            return [city for city in cities.values() if city.state_id == self.id]
