6bdfcbbb95fb4ce4abb2efe5d7d77a856aa51610
dariomnalerio <dariomnalerio@gmail.com>
2023-03-22 23:45:25 -0300

    Add all function, fix identation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
+from models.base_model import BaseModel
 from models.base_model import Base
 from models.user import User
 from models.place import Place
@@ -11,10 +12,11 @@ from models.amenity import Amenity
 from models.review import Review
 
 classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-            }
+    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+    'State': State, 'City': City, 'Amenity': Amenity,
+    'Review': Review
+}
+
 
 class DBStorage():
     """Class method"""
@@ -22,11 +24,21 @@ class DBStorage():
     __session = None
 
     def __init__(self):
-        self.__engine =  self.__engine = create_engine(('mysql+mysqldb://{}:{}@{}/{}')
-                                        .format(getenv('HBNB_MYSQL_USER'),
-                                                getenv('HBNB_MYSQL_PWD'),
-                                                getenv('HBNB_MYSQL_HOST'),
-                                                getenv('HBNB_MYSQL_DB')),
-                                        pool_pre_ping=True)
+
+        self.__engine = create_engine(('mysql+mysqldb://{}:{}@{}/{}')
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        objs = {}
+        for class_item in classes:
+            if classes[class_item] == cls or cls is None:
+                for key in self.__session.query(model[class_item]).all():
+                    objs[f"{key.__class__.__name__}.{key.id}"] = key
+        return objs
