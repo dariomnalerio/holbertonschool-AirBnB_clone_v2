4de1981ac8f89c0d56b359b826a2b868b4be193c
dariomnalerio <dariomnalerio@gmail.com>
2023-03-23 18:36:33 -0300

    Update amenity and state

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import Base 
+from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import relationship
@@ -9,6 +9,7 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
+
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == 'db':
