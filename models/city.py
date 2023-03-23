204c09f9d36d065d6de6dcc091e6a2ef41fd88bd
dariomnalerio <dariomnalerio@gmail.com>
2023-03-23 00:16:22 -0300

    Update city method

--- models/city.py
+++ models/city.py
@@ -2,12 +2,12 @@
 """ City Module for HBNB project """
 from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60),ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
