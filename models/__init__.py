050d4ae00bb2b14bde23737a2404f256f42c4df1
dariomnalerio <dariomnalerio@gmail.com>
2023-03-23 00:08:45 -0300

    Update init, now can initialize db

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
 
-storage = FileStorage()
-storage.reload()
+elif getenv('HBNB_TYPE_STORAGE') == 'file':
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
