c1100ec462e586317e9b79dc571c12e7aeee5af6
dariomnalerio <dariomnalerio@gmail.com>
2023-03-22 23:44:44 -0300

    Update city and state class

--- models/city.py
+++ models/city.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, DateTime
+from sqlalchemy import ForeignKey
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.base_model import Base 
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import relationship
