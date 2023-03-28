27a13ff69f610d12caebf15b162237b3e461f0f8
dariomnalerio <dariomnalerio@gmail.com>
2023-03-28 13:02:19 -0300

    Update state method getter

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
@@ -8,24 +9,12 @@ from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
 
     @property
-    def cities(self, id):
-        """ Get all cities of the db which are in the state """
-        from models import storage
-
-        storageType = os.environ.get("HBNB_TYPE_STORAGE")
-        if storageType == "db":
-            return self.cities
-
-        elif storageType == "file":
-            res = []
-            objs = storage.all()
-            for id, obj in objs.items():
-                if type(obj) is City and obj.state_id == id:
-                    res.append(obj)
-            return res
+    def cities(self):
+        from models.city import City
+        return [obj for obj in models.storage.all(City).values() if
+                obj.state_id == self.id]
